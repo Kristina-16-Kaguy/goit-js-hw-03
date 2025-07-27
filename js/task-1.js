bac07e34159b54d423a6e1fb3252915299b24f89
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-07-27 18:06:37 +0300

    add first task

---
+++ js/task-1.js
@@ -0,0 +1,27 @@
+// Задача 1. Генератор slug
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+function slugify(title) {
+  return title.split(" ").join("-").toLowerCase();
+}
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// Залиш цей код для перевірки ментором.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Оголошена функція slugify(title)
+// Виклик slugify("Arrays for beginners") повертає "arrays-for-beginners"
+// Виклик slugify("English for developer") повертає "english-for-developer"
+// Виклик slugify("Ten secrets of JavaScript") повертає "ten-secrets-of-javascript"
+// Виклик slugify("How to become a JUNIOR developer in TWO WEEKS") повертає "how-to-become-a-junior-developer-in-two-weeks"
